9086f16ff454cd2014d54294375a383bf729865f
Thong <thong.phan0601@hcmut.edu.vn>
2023-07-05 15:36:50 +0700

    Push

--- stm32 spi master slave/stm32_spi master/main.h
+++ stm32 spi master slave/stm32_spi master/main.h
@@ -4,9 +4,7 @@
 #include "stm32f10x.h"
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_gpio.h"
-#include "stm32f10x_usart.h"
 #include "stm32f10x_spi.h"
-#include "stm32f10x_adc.h"
 #include "string.h"
 
 
